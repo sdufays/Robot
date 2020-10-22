822c2d80decd1f3ea8de7ae01bb897687ac55d23
Nicholas Darosa <pl237431@ahschool.com>
2020-10-22 13:33:56 +0000

    Update scrum.py

--- scrum.py
+++ scrum.py
@@ -48,7 +48,7 @@ class robot():
         gpio.output(R2, True)
         self.LPWM.start(25*L_MULT)
         self.RPWM.start(25)
-        time.sleep(0.78)
+        time.sleep(0.72)
     
     def right(self):
         gpio.output(L1, True)
@@ -70,15 +70,16 @@ class robot():
 
 if __name__ == "__main__":
     robot1 = robot()
-    robot1.forward(1,15)
-    robot1.right()
-    robot1.forward(1,15)
     robot1.left()
-    robot1.forward(1,15)
-    robot1.right()
-    robot1.reverse(1,15)
-    robot1.right()
-    robot1.forward(1,15)
-    robot1.forward(1,15)
+    # robot1.forward(1,15)
+    # robot1.right()
+    # robot1.forward(1,15)
+    # robot1.left()
+    # robot1.forward(1,15)
+    # robot1.right()
+    # robot1.reverse(1,15)
+    # robot1.right()
+    # robot1.forward(1,15)
+    # robot1.forward(1,15)
     robot1.stop
     del robot1
