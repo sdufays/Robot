8851ec598b5970574e7ce65dda72259fcd43e872
Nicholas Darosa <pl237431@ahschool.com>
2020-10-27 13:33:06 +0000

    Update flask.py

---
+++ Flask/flask.py
@@ -0,0 +1,41 @@
+from flask import Flask, escape, request
+from robotClass import robot
+
+
+app = Flask(__name__)
+
+rc = robot()
+
+@app.route('/')
+def hello():
+    name = request.args.get("name", "World")
+    return f'Hello, {escape(name)}!'
+
+
+# move the robot fwd
+@app.route('/fwd')
+def fwd():
+    # rc.fwd(10)
+    return f'moved fwd !'
+
+# move the robot rev
+@app.route('/rev')
+def rev():
+    # rc.rev(10)
+    return f'moved rev !'
+
+# move the robot left
+@app.route('/left')
+def left():
+    # rc.left(10)
+    return f'moved left !'
+
+# move the robot right
+@app.route('/right')
+def right():
+    # rc.right(10)
+    return f'moved right !'
+
+
+
+app.run(host= '0.0.0.0', port=8080)
