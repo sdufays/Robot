5cefe6c24788a8cceab8ebd801851d5aeef6df20
Nicholas Darosa <pl237431@ahschool.com>
2020-11-04 14:15:56 +0000

    Update PWP2.py

--- Flask/PWP2.py
+++ Flask/PWP2.py
@@ -42,12 +42,12 @@ def run():
     time.sleep(0.5)
     rc.right()
     time.sleep(0.5)
-    # rc.forward(1.85, 15)
-    # time.sleep(0.5)
-    # rc.reverse(0.8, 15)
-    # time.sleep(0.5)
-    # rc.right()
-    # time.sleep(0.5)
+    rc.forward(1.85, 15)
+    time.sleep(0.5)
+    rc.reverse(0.8, 15)
+    time.sleep(0.5)
+    rc.right()
+    time.sleep(0.5)
     # rc.forward(3.1, 15)
     return "say something here so Alizain doesnt get mad mad"
 
