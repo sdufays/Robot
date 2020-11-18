f4b9c7bc135a8327e57fe1b50b19bc976caad42a
Nicholas Darosa <pl237431@ahschool.com>
2020-11-18 14:45:01 +0000

    Update client.java

--- JDK/client.java
+++ JDK/client.java
@@ -1,8 +1,32 @@
-HttpClient client = HttpClient.newHttpClient();
-HttpRequest request = HttpRequest.newBuilder()
-      .uri(URI.create("http://openjdk.java.net/"))
-      .build();
-client.sendAsync(request, BodyHandlers.ofString())
-      .thenApply(HttpResponse::body)
-      .thenAccept(System.out::println)
-      .join();
+class client {
+  public static void main(String[] args) {
+  }
+  public static void forward(){
+    HttpClient client = HttpClient.newHttpClient();
+  HttpRequest request = HttpRequest.newBuilder()
+    .uri(URI.create("192.168.1.116:8080/fwd"))
+    .build();
+  client.sendAsync(request, BodyHandlers.ofString());
+  }
+  public static void reverse(){
+    HttpClient client = HttpClient.newHttpClient();
+  HttpRequest request = HttpRequest.newBuilder()
+    .uri(URI.create("192.168.1.116:8080/rev"))
+    .build();
+  client.sendAsync(request, BodyHandlers.ofString());
+  }
+  public static void left(){
+    HttpClient client = HttpClient.newHttpClient();
+  HttpRequest request = HttpRequest.newBuilder()
+    .uri(URI.create("192.168.1.116:8080/left"))
+    .build();
+  client.sendAsync(request, BodyHandlers.ofString());
+  }
+  public static void right(){
+    HttpClient client = HttpClient.newHttpClient();
+  HttpRequest request = HttpRequest.newBuilder()
+    .uri(URI.create("192.168.1.116:8080/right"))
+    .build();
+  client.sendAsync(request, BodyHandlers.ofString());
+  }
+}
