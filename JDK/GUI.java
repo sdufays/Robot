18909f126ac86b9015affd37cba4644d211d957a
Alizain Fatehali <pl246901@ahschool.com>
2020-11-18 14:33:47 +0000

    Update JDK/GUI.java

--- JDK/GUI.java
+++ JDK/GUI.java
@@ -4,7 +4,8 @@ class gui{
        JFrame frame = new JFrame("My First GUI");
        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
        frame.setSize(300,300);
-       JButton button = new JButton("Press");
+       JButton button1 = new JButton("Button1");
+       JButton button2 = new JButton("Button 2")
        frame.getContentPane().add(button); // Adds Button to content pane of frame
        frame.setVisible(true);
     }
