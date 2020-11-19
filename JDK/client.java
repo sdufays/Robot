ea5209bb3e26c5366a9139cd163a5e283547ed09
Nicholas Darosa <pl237431@ahschool.com>
2020-11-19 14:45:39 +0000

    Update client.java

--- JDK/client.java
+++ JDK/client.java
@@ -1,32 +1,36 @@
-class client {
+
+import java.net.http.HttpClient;
+import java.lang.Object;
+
+class client extends Object {
   public static void main(String[] args) {
   }
   public static void forward(){
     HttpClient client = HttpClient.newHttpClient();
-  HttpRequest request = HttpRequest.newBuilder()
-    .uri(URI.create("192.168.1.116:8080/fwd"))
-    .build();
-  client.sendAsync(request, BodyHandlers.ofString());
+    HttpRequest request = HttpRequest.newBuilder()
+      .uri(URI.create("192.168.1.116:8080/fwd"))
+      .build();
+    client.sendAsync(request, BodyHandlers.ofString());
   }
   public static void reverse(){
     HttpClient client = HttpClient.newHttpClient();
-  HttpRequest request = HttpRequest.newBuilder()
-    .uri(URI.create("192.168.1.116:8080/rev"))
-    .build();
-  client.sendAsync(request, BodyHandlers.ofString());
+    HttpRequest request = HttpRequest.newBuilder()
+      .uri(URI.create("192.168.1.116:8080/rev"))
+      .build();
+    client.sendAsync(request, BodyHandlers.ofString());
   }
   public static void left(){
     HttpClient client = HttpClient.newHttpClient();
-  HttpRequest request = HttpRequest.newBuilder()
-    .uri(URI.create("192.168.1.116:8080/left"))
-    .build();
-  client.sendAsync(request, BodyHandlers.ofString());
+    HttpRequest request = HttpRequest.newBuilder()
+      .uri(URI.create("192.168.1.116:8080/left"))
+      .build();
+    client.sendAsync(request, BodyHandlers.ofString());
   }
   public static void right(){
     HttpClient client = HttpClient.newHttpClient();
-  HttpRequest request = HttpRequest.newBuilder()
-    .uri(URI.create("192.168.1.116:8080/right"))
-    .build();
-  client.sendAsync(request, BodyHandlers.ofString());
+    HttpRequest request = HttpRequest.newBuilder()
+      .uri(URI.create("192.168.1.116:8080/right"))
+      .build();
+    client.sendAsync(request, BodyHandlers.ofString());
   }
 }
