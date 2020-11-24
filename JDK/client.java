a5bec85e4e073bdafd1908de9adb89510325d6c4
Nicholas Darosa <pl237431@ahschool.com>
2020-11-24 13:59:10 +0000

    Update client.java

--- JDK/client.java
+++ JDK/client.java
@@ -1,6 +1,8 @@
+
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.net.URI;
 import java.net.http.HttpResponse.BodyHandlers;
 import java.lang.Object;
@@ -12,9 +14,12 @@ class client extends Object {
   public static void forward(){
     HttpClient client = HttpClient.newHttpClient();
     HttpRequest request = HttpRequest.newBuilder()
-      .uri(URI.create("192.168.1.116:8080/fwd"))
+      .uri(URI.create("http://192.168.1.116:8080/fwd"))
       .build();
-    client.sendAsync(request, BodyHandlers.ofString());
+    client.sendAsync(request, BodyHandlers.ofString())
+      .thenApply(HttpResponse::body)
+      .thenAccept(System.out::println)
+      .join();
   }
   public static void reverse(){
     HttpClient client = HttpClient.newHttpClient();
@@ -22,6 +27,9 @@ class client extends Object {
       .uri(URI.create("192.168.1.116:8080/rev"))
       .build();
     client.sendAsync(request, BodyHandlers.ofString());
+      .thenApply(HttpResponse::body)
+      .thenAccept(System.out::println)
+      .join();
   }
   public static void left(){
     HttpClient client = HttpClient.newHttpClient();
@@ -29,6 +37,9 @@ class client extends Object {
       .uri(URI.create("192.168.1.116:8080/left"))
       .build();
     client.sendAsync(request, BodyHandlers.ofString());
+      .thenApply(HttpResponse::body)
+      .thenAccept(System.out::println)
+      .join();
   }
   public static void right(){
     HttpClient client = HttpClient.newHttpClient();
@@ -36,5 +47,8 @@ class client extends Object {
       .uri(URI.create("192.168.1.116:8080/right"))
       .build();
     client.sendAsync(request, BodyHandlers.ofString());
+      .thenApply(HttpResponse::body)
+      .thenAccept(System.out::println)
+      .join();
   }
 }
