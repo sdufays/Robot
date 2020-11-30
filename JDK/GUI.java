706d838311895105c46dce4c0ddd35bb8aa5c8fd
Nicholas Darosa <pl237431@ahschool.com>
2020-11-30 14:45:35 +0000

    Update GUI.java

--- JDK/GUI.java
+++ JDK/GUI.java
@@ -1,30 +1,28 @@
+package test;
+
 import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import client.java;
 
-public class Main extends JFrame implements ActionListener {
-  int index = 0;
-  String[] msgs = { "test", "Left", "Right", "Test", "Forward", "Reverse"};
+public class GUI extends JFrame implements ActionListener {
+  String[] msgs = {"Left", "Right", "Forward", "Reverse"};
   JButton button1 = new JButton("Left");
   JButton button2 = new JButton("Right");
-  JButton button3 = new JButton(msgs[0]);
   JButton button4 = new JButton("Forward");
   JButton button5 = new JButton("Reverse");
 
-  public Main() {
+  public GUI() {
     BorderLayout layout = new BorderLayout();
     setLayout(layout);
     button1.addActionListener(this);
     button2.addActionListener(this);
-    button3.setEnabled(false);
     button4.addActionListener(this);
     button5.addActionListener(this);
     add(button1, BorderLayout.WEST);
     add(button2, BorderLayout.EAST);
-    add(button3, BorderLayout.CENTER);
     add(button4, BorderLayout.NORTH);
     add(button5, BorderLayout.SOUTH);
     setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -35,27 +33,18 @@ public class Main extends JFrame implements ActionListener {
   @Override
   public void actionPerformed(ActionEvent e) {
     Object obj = e.getSource();
-    if (obj == button1)
-    {
-      index = 1;
-      //TODO connect Client
+    if (obj == button1) {
+      client.left();
     }
-    else if (obj == button2)
-    {
-      index = 2;
-      //TODO connect Client
+    else if (obj == button2) {
+      client.right();
     }
-    else if (obj == button4)
-    {
-      index = 4;
-      //TODO connect Client
+    else if (obj == button4) {
+      client.forward();
     }
-    else if (obj == button5)
-    {
-      index = 5;
-      //TODO connect Client
+    else if (obj == button5) {
+      client.reverse();
     }
-    button3.setText(msgs[index]);
   }
 
   public static void main(String[] args) {
