f7de5866c792e0774197e4a42c30c9e6d9cfd485
Alizain Fatehali <pl246901@ahschool.com>
2020-11-30 14:27:24 +0000

    Update GUI.java

--- JDK/GUI.java
+++ JDK/GUI.java
@@ -1,104 +1,66 @@
-package layout;
-/*
- * BorderLayoutDemo.java
- *
- */
-import javax.swing.*;
 import java.awt.BorderLayout;
-import java.awt.Container;
-import java.awt.Dimension;
-import java.awt.*;
-import java.awt.event.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
 
-public abstract class BorderLayoutDemo implements ActionListener {
-    public static boolean RIGHT_TO_LEFT = false;
+public class Main extends JFrame implements ActionListener {
+  int index = 0;
+  String[] msgs = { "test", "Left", "Right", "Test", "Forward", "Reverse"};
+  JButton button1 = new JButton("Left");
+  JButton button2 = new JButton("Right");
+  JButton button3 = new JButton(msgs[0]);
+  JButton button4 = new JButton("Forward");
+  JButton button5 = new JButton("Reverse");
 
-    public static void addComponentsToPane(Container pane) {
+  public Main() {
+    BorderLayout layout = new BorderLayout();
+    setLayout(layout);
+    button1.addActionListener(this);
+    button2.addActionListener(this);
+    button3.setEnabled(false);
+    button4.addActionListener(this);
+    button5.addActionListener(this);
+    add(button1, BorderLayout.WEST);
+    add(button2, BorderLayout.EAST);
+    add(button3, BorderLayout.CENTER);
+    add(button4, BorderLayout.NORTH);
+    add(button5, BorderLayout.SOUTH);
+    setDefaultCloseOperation(EXIT_ON_CLOSE);
+    setSize(400, 200);
+    setVisible(true);
+  }
 
-        if (!(pane.getLayout() instanceof BorderLayout)) {
-            pane.add(new JLabel("Container doesn't use BorderLayout!"));
-            return;
-        }
-
-        if (RIGHT_TO_LEFT) {
-            pane.setComponentOrientation(
-                    java.awt.ComponentOrientation.RIGHT_TO_LEFT);
-        }
-
-        JButton button = new JButton("Stop");
-        pane.add(button, BorderLayout.PAGE_START);
-        // JButton button = new Jbutton("Forward");
-        // pane.add(button, BorderLayout.PAGE_START);
-
-        //Make the center component big, since that's the
-        //typical usage of BorderLayout.
-        JButton buttonf = new JButton("Forward");
-        buttonf.setPreferredSize(new Dimension(100, 200));
-        pane.add(buttonf, BorderLayout.CENTER);
-        buttonf.addActionListener(this);
-        public void actionPerformed(ActionEvent e)
-        {
-          // TODO import client.java;
-          // client.forward();
-        }
-
-
-        JButton buttonl = new JButton("Left");
-        buttonl.setPreferredSize(new Dimension(100,200));
-        pane.add(buttonl, BorderLayout.LINE_START);
-
-        JButton buttonrv = new JButton("Reverse");
-        buttonrv.setPreferredSize(new Dimension(300,100));
-        pane.add(buttonrv, BorderLayout.PAGE_END);
-
-        JButton buttonr = new JButton("Right");
-        buttonr.setPreferredSize(new Dimension(100,200));
-        pane.add(buttonr, BorderLayout.LINE_END);
+  @Override
+  public void actionPerformed(ActionEvent e) {
+    Object obj = e.getSource();
+    if (obj == button1)
+    {
+      index = 1;
+      //TODO connect Client
     }
-
-    /**
-     * Create the GUI and show it.  For thread safety,
-     * this method should be invoked from the
-     * event dispatch thread.
-     */
-    private static void createAndShowGUI() {
-
-        //Create and set up the window.
-        JFrame frame = new JFrame("SCRUMptious Java Client");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        //Set up the content pane.
-        addComponentsToPane(frame.getContentPane());
-        //Use the content pane's default BorderLayout. No need for
-        //setLayout(new BorderLayout());
-        //Display the window.
-        frame.pack();
-        frame.setVisible(true);
+    else if (obj == button2)
+    {
+      index = 2;
+      //TODO connect Client
     }
-
-    public static void main(String[] args) {
-        /* Use an appropriate Look and Feel */
-        try {
-            //UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
-            UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
-        } catch (UnsupportedLookAndFeelException ex) {
-            ex.printStackTrace();
-        } catch (IllegalAccessException ex) {
-            ex.printStackTrace();
-        } catch (InstantiationException ex) {
-            ex.printStackTrace();
-        } catch (ClassNotFoundException ex) {
-            ex.printStackTrace();
-        }
-        /* Turn off metal's use bold fonts */
-        UIManager.put("swing.boldMetal", Boolean.FALSE);
-
-        //Schedule a job for the event dispatch thread:
-        //creating and showing this application's GUI.
-        javax.swing.SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                createAndShowGUI();
-            }
-        });
+    else if (obj == button4)
+    {
+      index = 4;
+      //TODO connect Client
     }
+    else if (obj == button5)
+    {
+      index = 5;
+      //TODO connect Client
+    }
+    button3.setText(msgs[index]);
+  }
+
+  public static void main(String[] args) {
+    Main app = new Main();
+
+  }
+
 }
