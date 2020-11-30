d2e2a5191641bc954cdbb01aa51506d819593dd3
Nicholas Darosa <pl237431@ahschool.com>
2020-11-30 14:46:09 +0000

    Update client.java

--- JDK/client.java
+++ JDK/client.java
@@ -1,3 +1,5 @@
+package test;
+
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpRequest;
@@ -24,7 +26,7 @@ class client extends Object {
     HttpRequest request = HttpRequest.newBuilder()
       .uri(URI.create("192.168.1.116:8080/rev"))
       .build();
-    client.sendAsync(request, BodyHandlers.ofString());
+    client.sendAsync(request, BodyHandlers.ofString())
       .thenApply(HttpResponse::body)
       .thenAccept(System.out::println)
       .join();
@@ -34,7 +36,7 @@ class client extends Object {
     HttpRequest request = HttpRequest.newBuilder()
       .uri(URI.create("192.168.1.116:8080/left"))
       .build();
-    client.sendAsync(request, BodyHandlers.ofString());
+    client.sendAsync(request, BodyHandlers.ofString())
       .thenApply(HttpResponse::body)
       .thenAccept(System.out::println)
       .join();
@@ -44,7 +46,7 @@ class client extends Object {
     HttpRequest request = HttpRequest.newBuilder()
       .uri(URI.create("192.168.1.116:8080/right"))
       .build();
-    client.sendAsync(request, BodyHandlers.ofString());
+    client.sendAsync(request, BodyHandlers.ofString())
       .thenApply(HttpResponse::body)
       .thenAccept(System.out::println)
       .join();
