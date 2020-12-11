1a4ffd776c3153f74fe86b0f584a0c111954ccec
Nicholas Darosa <pl237431@ahschool.com>
2020-12-11 14:43:17 +0000

    Update PWP2.py

--- Flask/PWP2.py
+++ Flask/PWP2.py
@@ -14,13 +14,13 @@ def menu():
 # move the robot fwd
 @app.route('/fwd')
 def fwd():
-    rc.forward(0.05, 60)
+    rc.forward(0.05, 65)
     return 'moved fwd!'
 
 # move the robot rev
 @app.route('/rev')
 def rev():
-    rc.reverse(0.05, 60)
+    rc.reverse(0.05, 65)
     return 'moved rev!'
 
 # move the robot left
