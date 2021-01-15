027f43dd8c1631d9d5d920c6755d316ce9550505
Alizain Fatehali <pl246901@ahschool.com>
2021-01-15 14:44:43 +0000

    Update PWP2.py

--- Flask/PWP2.py
+++ Flask/PWP2.py
@@ -1,5 +1,6 @@
 from flask import Flask, escape, request, render_template, Response
 from robotClass import robot
+from camera import Camera
 import time
 import threading
 import os
@@ -60,5 +61,22 @@ def run():
     rc.forward(float(f), 15)
     return "robot work yes"
 
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen(Camera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
 
 app.run(host= '0.0.0.0', port=8080)
