17ab3771e32afbd078f7bf876fa2df8eedeb9b10
Alizain Fatehali <pl246901@ahschool.com>
2021-01-19 14:17:20 +0000

    Update cameratest.py

--- Camera/cameratest.py
+++ Camera/cameratest.py
@@ -1,28 +1,22 @@
-#Modified by smartbuilds.io
-#Date: 27.09.20
-#Desc: This scrtipt script..
+from flask import Flask, render_template, Response
+from camera import Camera
 
-import cv2
-from imutils.video.pivideostream import PiVideoStream
-import imutils
-import time
-import numpy as np
+app = Flask(__name__)
 
-class VideoCamera(object):
-    def __init__(self, flip = False):
-        self.vs = PiVideoStream().start()
-        self.flip = flip
-        time.sleep(2.0)
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-    def __del__(self):
-        self.vs.stop()
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
-    def flip_if_needed(self, frame):
-        if self.flip:
-            return np.flip(frame, 0)
-        return frame
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen(Camera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
 
-    def get_frame(self):
-        frame = self.flip_if_needed(self.vs.read())
-        ret, jpeg = cv2.imencode('.jpg', frame)
-        return jpeg.tobytes()
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
