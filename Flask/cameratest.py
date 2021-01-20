534c61f0fe38f0aec3502828caaff8b13e5a9e5f
Alizain Fatehali <pl246901@ahschool.com>
2021-01-20 14:15:14 +0000

    Update cameratest.py

--- Flask/cameratest.py
+++ Flask/cameratest.py
@@ -1,26 +1,28 @@
-# import required modules
-from flask import Flask, render_template, Response 
-import picamera 
-import cv2
-import socket 
-import io 
-app = Flask(__name__) 
-vc = cv2.VideoCapture(0) 
-@app.route('/') 
-def index(): 
-   """Video streaming .""" 
-   return render_template('index.html') 
-def gen(): 
-   """Video streaming generator function.""" 
-   while True: 
-       rval, frame = vc.read() 
-       cv2.imwrite('pic.jpg', frame) 
-       yield (b'--frame\r\n' 
-              b'Content-Type: image/jpeg\r\n\r\n' + open('pic.jpg', 'rb').read() + b'\r\n') 
-@app.route('/video_feed') 
-def video_feed(): 
-   """Video streaming route. Put this in the src attribute of an img tag.""" 
-   return Response(gen(), 
-                   mimetype='multipart/x-mixed-replace; boundary=frame') 
-if __name__ == '__main__': 
-	app.run(host='0.0.0.0', debug=True, threaded=True) 
+#!/usr/bin/python3
+from importlib import import_module
+import os
+from flask import Flask, render_template, Response
+from VideoStream.camera_pi import Camera
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+   return render_template('index.html')
+def gen(camera):
+
+while True:
+    frame = camera.get_frame()
+    yield (b'--frame\r\n'
+           b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+@app.route('/video_feed')
+def video_feed():
+
+    return Response(gen(Camera()),
+                mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', threaded=True, debug=True)
