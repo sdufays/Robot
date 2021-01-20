097f85000e0c1d63292d89e038831a9b94e93a57
Alizain Fatehali <pl246901@ahschool.com>
2021-01-20 14:18:02 +0000

    Update cameratest.py

--- Flask/cameratest.py
+++ Flask/cameratest.py
@@ -12,16 +12,15 @@ app = Flask(__name__)
 def index():
    return render_template('index.html')
 def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+            b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+    @app.route('/video_feed')
+    def video_feed():
 
-while True:
-    frame = camera.get_frame()
-    yield (b'--frame\r\n'
-           b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-@app.route('/video_feed')
-def video_feed():
-
-    return Response(gen(Camera()),
-                mimetype='multipart/x-mixed-replace; boundary=frame')
+        return Response(gen(Camera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
 if __name__ == '__main__':
