7209420363c02bf633c05ce5f334f20e2f771721
Alizain Fatehali <pl246901@ahschool.com>
2021-01-20 14:25:41 +0000

    Update cameratest.py

--- Flask/cameratest.py
+++ Flask/cameratest.py
@@ -8,10 +8,8 @@ from http import server
 PAGE="""\
 <html>
 <head>
-<title>Raspberry Pi - Live Camera</title>
 </head>
 <body>
-<center><h1>Raspberry Pi - Live Camera</h1></center>
 <center><img src="stream.mjpg" width="640" height="480"></center>
 </body>
 </html>
