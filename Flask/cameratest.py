b85b35a64b22c0ac99cd07cafd59495a617c25b2
Alizain Fatehali <pl246901@ahschool.com>
2021-01-21 14:23:58 +0000

    Update cameratest.py

--- Flask/cameratest.py
+++ Flask/cameratest.py
@@ -22,7 +22,7 @@ PAGE="""\
 </style>
 <body>
     <div id="NW">
-    <img src="stream.mjpg" width="1000" height="500"></div>
+    <img src="stream.mjpg" width="1030" height="490"></div>
     <div id="NE"></div>
     <div id="SE"></div>​
     <div id="SW"></div>
