01dff9535be3360891e044eaa7902d55f28e4888
Alizain Fatehali <pl246901@ahschool.com>
2021-01-27 14:13:39 +0000

    Update cameratest.py

--- Flask/cameratest.py
+++ Flask/cameratest.py
@@ -89,7 +89,7 @@ class StreamingServer(socketserver.ThreadingMixIn, server.HTTPServer):
     allow_reuse_address = True
     daemon_threads = True
 
-with picamera.PiCamera(resolution='1280x720', framerate=60) as camera:
+with picamera.PiCamera(resolution='960x540', framerate=60) as camera:
     output = StreamingOutput()
     camera.rotation = 180
     camera.start_recording(output, format='mjpeg')
