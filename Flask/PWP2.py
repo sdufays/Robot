9e90e30fddf8463fd49b06ce2228c26f51dec304
Alizain Fatehali <pl246901@ahschool.com>
2021-01-28 14:15:17 +0000

    Update PWP2.py

--- Flask/PWP2.py
+++ Flask/PWP2.py
@@ -1,13 +1,7 @@
-from flask import Flask, escape, request, render_template
+from flask import Flask, escape, request, render_template, Response
 from robotClass import robot
 import time
-import io
-import picamera
-import logging
-import socketserver
-from threading import Condition
-from http import server
-
+import cv2
 
 app = Flask(__name__)
 rc = robot()
@@ -64,6 +58,21 @@ def run():
     rc.forward(float(f), 15)
     return "robot work yes"
 
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+def gen():
+    camera = cv2.VideoCapture(0)
+
+    while True:
+        ret, img = camera.read()
+
+        if ret:
+            frame = cv2.imencode('.jpg', img)[1].tobytes()
+            yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+        else:
+            break
 
 
 app.run(host= '0.0.0.0', port=8080)
