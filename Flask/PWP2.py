823c911edd88f655e2f43e3e60857e9132788832
Alizain Fatehali <pl246901@ahschool.com>
2021-01-29 14:32:58 +0000

    Update PWP2.py

--- Flask/PWP2.py
+++ Flask/PWP2.py
@@ -2,31 +2,18 @@ from flask import Flask, escape, request, render_template, Response
 from robotClass import robot
 import time
 import picamera
-import cv2
 import socket
 import io
 
 app = Flask(__name__)
 rc = robot()
-vc = cv2.VideoCapture(0)
+camera = picamera.camera
 
 #fwd 3, right 0.75, fwd 1.75, rev 0.8, right 0.75, fwd 3.05
-@app.route('/stream')
-def index(): 
-   """Video streaming .""" 
-   return render_template('index.html') 
-def gen(): 
-   """Video streaming generator function.""" 
-   while True: 
-       rval, frame = vc.read() 
-       cv2.imwrite('pic.jpg', frame) 
-       yield (b'--frame\r\n' 
-              b'Content-Type: image/jpeg\r\n\r\n' + open('pic.jpg', 'rb').read() + b'\r\n') 
-@app.route('/video_feed') 
-def video_feed(): 
-   """Video streaming route. Put this in the src attribute of an img tag.""" 
-   return Response(gen(), 
-                   mimetype='multipart/x-mixed-replace; boundary=frame') 
+
+@app.route('/streamtest')
+def stream():
+    Frame = picamera.capture()
 
 
 @app.route('/')
