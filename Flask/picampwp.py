4f29aab2ef92b84580539496bcc2e901f270e02a
Alizain Fatehali <pl246901@ahschool.com>
2021-02-01 02:07:21 +0000

    Update picampwp.py

--- Flask/picampwp.py
+++ Flask/picampwp.py
@@ -1,1 +1,96 @@
+from flask import Flask, escape, request, render_template, Response
+from robotClass import robot
+import time
+import socket
+import io
+import logging
+from flask import Flask, render_template, Response
+from camera import Camera
 
+#maybe???
+#https://raspberrypi.stackexchange.com/questions/42759/streaming-raspberry-pi-camera-to-html-webpage-using-picamera-and-flask
+
+# This is the real one:
+# https://blog.miguelgrinberg.com/post/video-streaming-with-flask
+
+app = Flask(__name__)
+rc = robot()
+#fwd 3, right 0.75, fwd 1.75, rev 0.8, right 0.75, fwd 3.05
+
+@app.route('/plswork')
+def index():
+    return render_template('index.html')
+
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen(Camera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/')
+def home():
+    return render_template("home.html")
+
+# move the robot fwd
+@app.route('/fwd', methods=['POST'])
+def fwd():
+    rc.forward(0.1, 30)
+    return render_template("home.html")
+
+# move the robot rev
+@app.route('/rev', methods=['POST'])
+def rev():
+    rc.reverse(0.1, 30)
+    return render_template("home.html")
+
+# move the robot left
+@app.route('/left', methods=['POST'])
+def left():
+    rc.left(0.05)
+    return render_template("home.html")
+
+# move the robot right
+@app.route('/right', methods=['POST'])
+def right():
+    rc.right(0.05)
+    return render_template("home.html")
+
+# run the predetermined course
+@app.route('/run')
+def run():
+    a = request.args.get('a')
+    b = request.args.get('b')
+    c = request.args.get('c')
+    d = request.args.get('d')
+    e = request.args.get('e')
+    f = request.args.get('f')
+    rc.forward(float(a), 15)
+    time.sleep(0.5)
+    rc.right(float(b))
+    time.sleep(0.5)
+    rc.forward(float(c), 15)
+    time.sleep(0.5)
+    rc.reverse(float(d), 15)
+    time.sleep(0.5)
+    rc.right(float(e))
+    time.sleep(0.5)
+    rc.forward(float(f), 15)
+    return "robot work yes"
+
+# @app.route('/streamlog')
+# def terminal():
+#     logFormatter = logging.Formatter("%(asctime)s [%(levelname)-5.5s] %(message)s")
+#     rootLogger = logging.getLogger()
+
+#     fileHandler = logging.FileHandler(loggingfile)
+#     fileHandler.setFormatter(logFormatter)
+
+#     consoleHandler = 
+
+
+app.run(host= '0.0.0.0', port=8080)
