6d72c8e4d8cb2095932f7873b517910b4de969e2
Sarah Dufays <pl247711@ahschool.com>
2021-03-01 14:20:30 +0000

    Update camera_pi.py

--- Flask/camera_pi.py
+++ Flask/camera_pi.py
@@ -1,16 +1,9 @@
-# !/usr/bin/env python
-# -*- coding: utf-8 -*-
-#
-#  camera_pi.py
-#  
-#  
-#  
 import time
 import io
 import threading
 import picamera
-import cv2 
-
+import cv2
+import time
 
 class Camera(object):
     thread = None  # background thread that reads frames from camera
@@ -32,6 +25,9 @@ class Camera(object):
         self.initialize()
         return self.frame
 
+    def StopPreview():
+        Camera.stop_camera = True
+
     @classmethod
     def _thread(cls):
         with picamera.PiCamera() as camera:
@@ -49,52 +45,54 @@ class Camera(object):
                                                  use_video_port=True):
                 # store frame
                 stream.seek(0)
+                
+
+                background = cv2.imread('image.jpg')
+                height, width, _ = background.shape
+                overlay = background.copy()
+                radius = 80
+
+                cv2.circle(overlay,
+                                (width-radius,height-radius),
+                                radius,
+                                (0, 255, 255),
+                                -1,
+                                8)
+                cv2.arrowedLine(overlay,
+                                (width-radius,height-radius),
+                                (width-radius,height-2*radius),
+                                (0,0,255),
+                                8)
+
+                cv2.circle(overlay,
+                                (radius,height-radius),
+                                radius,
+                                (0, 255, 255),
+                                -1,
+                                8)
+                
+                cv2.arrowedLine(overlay,
+                                (radius,height-radius),
+                                (radius,height-2*radius),
+                                (0,0,255),
+                                8)
+                    
+
+                added_image = cv2.addWeighted(background,1,overlay,0.5,0)
+
+                cv2.imwrite('combined.jpg', added_image)
+
+
                 cls.frame = stream.read()
 
                 # reset stream for next frame
                 stream.seek(0)
+                stream.truncate()
 
-            background = cv2.imread('image.jpg')
-            height, width, _ = background.shape
-            overlay = background.copy()
-            radius = 80
-
-            cv2.circle(overlay,
-                            (width-radius,height-radius),
-                            radius,
-                            (0, 255, 255),
-                            -1,
-                            8)
-            cv2.arrowedLine(overlay,
-                            (width-radius,height-radius),
-                            (width-radius,height-2*radius),
-                            (0,0,255),
-                            8)
-
-            cv2.circle(overlay,
-                            (radius,height-radius),
-                            radius,
-                            (0, 255, 255),
-                            -1,
-                            8)
-            
-            cv2.arrowedLine(overlay,
-                            (radius,height-radius),
-                            (radius,height-2*radius),
-                            (0,0
+                # if there hasn't been any clients asking for frames in
+                # the last 10 seconds stop the thread
+                if time.time() - cls.last_access > 10:
+                    break
+        cls.thread = None
 
 
-            added_image = cv2.addWeighted(background,1,overlay,0.5,0)
-            cv2.imwrite('combined.jpg', added_image)
-
-            cls.frame = stream.read()
-
-            # reset stream for next frame
-            stream.seek(0)
-            stream.truncate()
-
-            # if there hasn't been any clients asking for frames in
-            # the last 10 seconds stop the thread
-            if time.time() - cls.last_access > 10:
-                break
-    cls.thread = None
