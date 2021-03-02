e278dc71aa0dedf6ea031c6bbaddeb421a901f96
Sarah Dufays <pl247711@ahschool.com>
2021-03-02 14:21:32 +0000

    Update camera_pi.py

--- Flask/camera_pi.py
+++ Flask/camera_pi.py
@@ -41,6 +41,42 @@ class Camera(object):
                                                  use_video_port=True):
                 # store frame
                 stream.seek(0)
+                
+                background = cv2.imread('image.jpg')
+                height, width, _ = background.shape
+                overlay = background.copy()
+                radius = 80
+
+                cv2.circle(overlay,
+                                (width-radius,height-radius),
+                                radius,
+                                (0, 255, 255),
+                                -1,
+                                8)
+                cv2.arrowedLine(overlay,
+                                (width-radius,height-radius),
+                                (width-radius,height-2*radius),
+                                (0,0,255),
+                                8)
+
+                cv2.circle(overlay,
+                                (radius,height-radius),
+                                radius,
+                                (0, 255, 255),
+                                -1,
+                                8)
+                
+                cv2.arrowedLine(overlay,
+                                (radius,height-radius),
+                                (radius,height-2*radius),
+                                (0,0,255),
+                                8)
+                    
+
+                added_image = cv2.addWeighted(background,1,overlay,0.5,0)
+
+                cv2.imwrite('combined.jpg', added_image)
+
                 cls.frame = stream.read()
 
                 # reset stream for next frame
