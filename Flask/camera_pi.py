610db92fecd0abe190982d10642b3543f31f4df5
Alizain Fatehali <pl246901@ahschool.com>
2021-03-02 14:08:14 +0000

    Update camera_pi.py

--- Flask/camera_pi.py
+++ Flask/camera_pi.py
@@ -91,8 +91,8 @@ class Camera(object):
 
             # if there hasn't been any clients asking for frames in
             # the last 10 seconds stop the thread
-            if time.time() - cls.last_access > 10:
-                break
+            # if time.time() - cls.last_access > 10:
+            #     break
     cls.thread = None
 
 
