bd160d7c54b02c1394788384e4d3d564b57bab06
Sarah Dufays <pl247711@ahschool.com>
2021-03-04 23:30:19 +0000

    Update camera_pi.py

--- Flask/camera_pi.py
+++ Flask/camera_pi.py
@@ -1,21 +1,26 @@
 import cv2
 
+# main class
 class VideoCamera(object):
     def __init__(self):
         self.video = cv2.VideoCapture(0)
+        # cv2 stream capture
     
     def __del__(self):
         self.video.release()
     
-    def get_frame(self, x1, x2):
+    # overlay function
+    def get_frame(self, left_c, right_c):
         success, image = self.video.read()
+        radius = 80
         image = cv2.flip(image, 0)
         image = cv2.flip(image, 1)
-
+        # creates the dimensions for the stream
         background = image
         height, width, _ = background.shape
         overlay = background.copy()
-        radius = 80
+
+        # first left circle
         cv2.circle(overlay,
                         (width-radius,height-radius),
                         radius,
@@ -23,64 +28,66 @@ class VideoCamera(object):
                         -1,
                         8)
 
-        if x1 == 1: 
+        if left_c == 1: 
             cv2.arrowedLine(overlay,
                 (width-radius,height-radius),
                 (width-radius,height-2*radius),
                 (255, 74, 219),
                 8)
-        if x1 == 2:
+        if left_c == 2:
             cv2.arrowedLine(overlay,
                 (width-radius,height-radius),
                 (width-2*radius,height-radius),
                 (255, 74, 219),
                 8)
-        if x1 == 3: #backwards
+        if left_c == 3: 
             cv2.arrowedLine(overlay,
                 (width-radius,height-radius),
                 (width-radius,height),
                 (255, 74, 219),
                 8)
-        if x1 == 4: #this is right
+        if left_c == 4: 
             cv2.arrowedLine(overlay,
                 (width-radius,height-radius),
                 (width,height-radius),
                 (255, 74, 219),
                 8)
-        
+
+        # second right circle
         cv2.circle(overlay,
                         (radius,height-radius),
                         radius,
                         (74, 204, 224),
                         -1,
-                        8) #left circle
+                        8) 
 
-        if x2 == 1: #forward
+        if right_c == 1: 
             cv2.arrowedLine(overlay,
                             (radius,height-radius),
                             (radius,height-2*radius),
                             (0,0,255),
                             8)
-        if x2 == 2: 
+        if right_c == 2: 
             cv2.arrowedLine(overlay,
                 (radius,height-radius),
                 (0,height-radius),
                 (0,0,255),
                 8)
-        if x2 == 3: 
+        if right_c == 3: 
             cv2.arrowedLine(overlay,
                 (radius,height-radius),
                 (radius,height),
                 (0,0,255),
                 8)
-        if x2 == 4: 
+        if right_c == 4: 
             cv2.arrowedLine(overlay,
                 (radius,height-radius),
                 (2*radius,height-radius),
                 (0,0,255),
                 8)
-
+        # combine the overlay and the stream
         added_image = cv2.addWeighted(background,1,overlay,1,0)
+        # convert image to jpg and return 
         ret, jpeg = cv2.imencode('.jpg', added_image)
         return jpeg.tobytes()
 
