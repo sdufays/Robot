c4ef8dc01ff0006efa49e19f6ee9af5c685fcbc8
Sarah Dufays <pl247711@ahschool.com>
2021-03-04 14:39:27 +0000

    Update camera_pi.py

--- Flask/camera_pi.py
+++ Flask/camera_pi.py
@@ -1,15 +1,8 @@
-
 import cv2
 
 class VideoCamera(object):
     def __init__(self):
-        # Using OpenCV to capture from device 0. If you have trouble capturing
-        # from a webcam, comment the line below out and use a video file
-        # instead.
         self.video = cv2.VideoCapture(0)
-        # If you decide to use video.mp4, you must have this file in the folder
-        # as the main.py.
-        # self.video = cv2.VideoCapture('video.mp4')
     
     def __del__(self):
         self.video.release()
@@ -18,9 +11,6 @@ class VideoCamera(object):
         success, image = self.video.read()
         image = cv2.flip(image, 0)
         image = cv2.flip(image, 1)
-        # We are using Motion JPEG, but OpenCV defaults to capture raw images,
-        # so we must encode it into JPEG in order to correctly display the
-        # video stream.
 
         background = image
         height, width, _ = background.shape
@@ -31,21 +21,21 @@ class VideoCamera(object):
                         radius,
                         (0, 255, 255),
                         -1,
-                        8) #this is right circle
+                        8) #right circle
 
-        if chance1 == 1: #this is forward
+        if chance1 == 1: #forward
             cv2.arrowedLine(overlay,
                 (width-radius,height-radius),
                 (width-radius,height-2*radius),
                 (0,0,255),
                 8)
-        if chance1 == 2: #this is left
+        if chance1 == 2: #left
             cv2.arrowedLine(overlay,
                 (width-radius,height-radius),
                 (width-2*radius,height-radius),
                 (0,0,255),
                 8)
-        if chance1 == 3: #this is backward
+        if chance1 == 3: #backwards
             cv2.arrowedLine(overlay,
                 (width-radius,height-radius),
                 (width-radius,height),
@@ -63,27 +53,27 @@ class VideoCamera(object):
                         radius,
                         (0, 255, 255),
                         -1,
-                        8) #this is left circle
+                        8) #left circle
 
-        if chance2 == 1: #this is forward
+        if chance2 == 1: #forward
             cv2.arrowedLine(overlay,
                             (radius,height-radius),
                             (radius,height-2*radius),
                             (0,0,255),
                             8)
-        if chance2 == 2: #this is left
+        if chance2 == 2: #left
             cv2.arrowedLine(overlay,
                 (radius,height-radius),
                 (0,height-radius),
                 (0,0,255),
                 8)
-        if chance2 == 3: #this is backward
+        if chance2 == 3: #backward
             cv2.arrowedLine(overlay,
                 (radius,height-radius),
                 (radius,height),
                 (0,0,255),
                 8)
-        if chance2 == 4: #this is right
+        if chance2 == 4: #tright
             cv2.arrowedLine(overlay,
                 (radius,height-radius),
                 (2*radius,height-radius),
