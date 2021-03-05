72ee0524d4511e844995553c0160804f80d89885
Sarah Dufays <pl247711@ahschool.com>
2021-03-05 14:41:43 +0000

    Update camera_pi.py

--- Flask/camera_pi.py
+++ Flask/camera_pi.py
@@ -25,9 +25,9 @@ class VideoCamera(object):
 
         if left_c == 1: 
             # cv2.arrowedLine(overlay, (500, 600), (500, 700), (64, 236, 255), 3, 8, 0, 0.1)
-            cv2.arrowedLine(overlay, (width-radius,height-radius), (width-radius,height-2*radius),(64, 236, 255),3,8)
+            cv2.arrowedLine(overlay, (width-radius,height-radius), (width-radius,height-2*radius),(64, 236, 255),3,8, 0, 0.1)
         if left_c == 2:
-            cv2.arrowedLine(overlay,(width-radius,height-radius), (width-2*radius,height-radius),(64, 236, 255),8)
+            cv2.arrowedLine(overlay,(width-radius,height-radius), (width-2*radius,height-radius),(64, 236, 255),8,0, 0.1)
         if left_c == 3: 
             cv2.arrowedLine(overlay,(width-radius,height-radius),(width-radius,height),(64, 236, 255),8)
         if left_c == 4: 
