0d35181ecad16946621c9869868c7a70e56298ef
Sarah Dufays <pl247711@ahschool.com>
2021-03-08 14:29:10 +0000

    Update camera_pi.py

--- Flask/camera_pi.py
+++ Flask/camera_pi.py
@@ -16,12 +16,11 @@ class VideoCamera(object):
         image = cv2.flip(image, 0)
         image = cv2.flip(image, 1)
         # creates the dimensions for the stream
-        background = image
-        height, width, _ = background.shape
-        overlay = background.copy()
+        height, width, _ = image.shape
+        overlay = image.copy()
 
         # first left circle
-        cv2.circle(overlay,(width-radius,height-radius), radius, (147, 64, 255), -1, 8)
+        cv2.circle(overlay,(width-radius,height-radius), radius, (147, 64, 255), -2, 8)
 
         if left_c == 1: 
             cv2.arrowedLine(overlay, (width-radius,height-radius), (width-radius,height-2*radius),(217, 159, 0),3,8, 0, 0.2)
@@ -44,7 +43,7 @@ class VideoCamera(object):
         if right_c == 4: 
             cv2.arrowedLine(overlay,(radius,height-radius),(2*radius,height-radius),(217, 159, 0),3,8,0, 0.2)
         # combine the overlay and the stream
-        added_image = cv2.addWeighted(background,1,overlay,1,0)
+        added_image = cv2.addWeighted(image,1,overlay,1,0)
         # convert image to jpg and return 
         ret, jpeg = cv2.imencode('.jpg', added_image)
         return jpeg.tobytes()
